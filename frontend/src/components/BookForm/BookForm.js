6c47f52ac4cfbc66c4de9b89bcde863c309047c0
Shr4528 <Shr4528@gmail.com>
2023-11-26 15:22:32 -0600

    app

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookForm = () => {
+    return (
+        <div className="app-block block-form">BookList
+            <h2>
+                Add a New Book
+            </h2>
+        </div>
+    );
+};
+
+export default BookForm;
---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Filter = () => {
+    return (
+        <div className="app-block filter">
+            Filter
+        </div>
+    );
+};
+
+export default Filter;
