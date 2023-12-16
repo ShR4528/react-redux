cf2dedfe06d4a09a49f7fd0b3cdea8b9b7045af4
Shr4528 <Shr4528@gmail.com>
2023-12-15 19:25:15 -0600

    filterSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/reducer';
+import filterSlice from './slices/filterSlice';
 
 
 const store = configureStore({
     reducer: {
         books: booksReducer,
-
-
+        filter: filterSlice
     },
 });
 
