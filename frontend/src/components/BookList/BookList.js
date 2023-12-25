1896bda0a5ce21e74e3a14f3f935215fb1896c97
Shr4528 <Shr4528@gmail.com>
2023-12-25 15:18:11 -0600

    highlight filter

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -12,6 +12,23 @@ const BookList = () => {
     const authorFilter = useSelector(selectAuthorFilter);
     const onlyFavoriteFilter = useSelector(selectOnlyFavorite);
 
+    const highlightMatch = (text, filter) => {
+        if (!filter) return text;
+        const regex = new RegExp(`(${filter})`, 'gi');
+        return text.split(regex).map((substring, i) => {
+            if (substring.toLowerCase() === filter.toLowerCase()) {
+                return (
+                    <span key={i} className="highlight">
+                        {substring}
+                    </span>
+                );
+            }
+            return substring;
+
+        });
+
+    };
+
 
     const handleDeleteBook = (id) => {
         dispatch(deleteBook(id));
@@ -50,7 +67,8 @@ const BookList = () => {
                     {filterBooks.map((book, i) => (
                         <li key={book.id}>
                             <div className="book-info">
-                                {++i}.  {book.title} by <strong>{book.author}</strong>
+                                {++i}. {highlightMatch(book.title, titleFilter)}
+                                by <strong>{highlightMatch(book.author, authorFilter)}</strong>
                             </div>
                             <div className="book-actions">
                                 <span onClick={() => handleToggleFavorite(book.id)}>
