516dbc0d741ed4a77f5d468958ebd1ca8a1a188a
Shr4528 <Shr4528@gmail.com>
2024-01-16 11:41:30 -0600

    dispatch error

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -18,7 +18,7 @@ export const fetchBook = createAsyncThunk('books/fetchBook',
             const res = await axios.get(url);
             return res.data;
         } catch (error) {
-            thunkApi.dispatch(setError('error.message'));
+            thunkApi.dispatch(setError(error.message));
             throw error;
         }
 
@@ -32,12 +32,9 @@ const booksSlice = createSlice({
         addBook: (state, action) => {
             state.books.push(action.payload);
         },
-
-        // Удаляем книгу по id из массива состояния
         deleteBook: (state, action) => {
             return state.filter((book) => book.id !== action.payload);
         },
-
         toggleFavorite: (state, action) => {
             state.books.forEach((book) => {
                 if (book.id === action.payload) {
