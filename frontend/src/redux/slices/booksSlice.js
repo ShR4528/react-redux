d4b81d8d748da53549350c17aafefb195286222e
Shr4528 <Shr4528@gmail.com>
2024-01-16 11:46:51 -0600

    change extrareducer

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -44,14 +44,14 @@ const booksSlice = createSlice({
 
         },
     },
-    extraReducers: (builder) => {
-        builder.addCase(fetchBook.fulfilled, (state, action) => {
+    extraReducers: {
+        [fetchBook.fulfilled]: (state, action) => {
             if (action.payload.title && action.payload.author) {
-                state.push(createBookWithID(action.payload, "Api"));
+                state.books.push(createBookWithID(action.payload, "Api"));
             }
-        });
-
+        }
     }
+
 });
 
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
